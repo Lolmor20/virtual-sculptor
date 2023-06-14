7a533c0b5420963055f71cce9478455998467891
Lolmor20 <62568741+Lolmor20@users.noreply.github.com>
2023-06-14 18:30:42 +0200

    multiple changes, reverting changes concerning parabox

--- Assets/Scripts/Actions/Sculpting.cs
+++ Assets/Scripts/Actions/Sculpting.cs
@@ -1,16 +1,7 @@
-﻿using UnityEngine;
-using System.Linq;
-using Assets.Scripts.Managers;
-using Parabox.CSG;
-using System;
-
-namespace Assets.Scripts.Actions
+﻿namespace Assets.Scripts.Actions
 {
     public class Sculpting : Action
     {
-        private readonly GameObject multiTool = FlystickManager.Instance.MultiTool;
-        private BooleanOperation currentOperation = BooleanOperation.SUBTRACT;
-        private GameObject[] gameObjects;
         private bool sculpting = false;
 
         public enum BooleanOperation
@@ -32,53 +23,10 @@ namespace Assets.Scripts.Actions
 
         public override void Update()
         {
-            if (sculpting)
-            {
-                Bounds multiToolBounds = multiTool.GetComponent<Collider>().bounds;
-                var intersectingObjects = from item
-                                          in gameObjects
-                                          where ((item.GetComponent<Collider>() != null) && (multiToolBounds.Intersects(item.GetComponent<Collider>().bounds)))
-                                          select item;
-                var list = intersectingObjects.ToList();
-
-                foreach (var obj in list)
-                {
-                    try
-                    {
-                        Model result = performBooleanOperation(obj);
-
-                        //obj.GetComponent<MeshFilter>().mesh = result.mesh;
-                        //obj.GetComponent<MeshCollider>().sharedMesh = result.mesh;
-
-                        var composite = new GameObject();
-                        composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
-                        composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
-                    } catch (Exception e)
-                    {
-                        Debug.Log(e);
-                    }
-                }
-            }
-        }
-
-        private Model performBooleanOperation(GameObject obj)
-        {
-            switch (currentOperation)
-            {
-                case BooleanOperation.UNION:
-                    return CSG.Union(obj, multiTool);
-                case BooleanOperation.SUBTRACT:
-                    return CSG.Subtract(obj, multiTool);
-                case BooleanOperation.INTERSECT:
-                    return CSG.Intersect(obj, multiTool);
-                default:
-                    return CSG.Subtract(obj, multiTool);
-            }
         }
 
         public override void Init()
         {
-            gameObjects = GameObject.FindGameObjectsWithTag(GlobalVars.UniversalTag);
         }
 
         public override void Finish()
@@ -87,7 +35,7 @@ namespace Assets.Scripts.Actions
 
         public void SetCurrentOperation(BooleanOperation operation)
         {
-            this.currentOperation = operation;
+            //this.currentOperation = operation;
         }
     }
 }
